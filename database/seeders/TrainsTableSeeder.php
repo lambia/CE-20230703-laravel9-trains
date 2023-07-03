cc29b6b6d17de6aab0baf2e294c7cf19670fd180
Luca Lambiase <luca.lambia@gmail.com>
2023-07-03 10:50:06 +0200

    correzione

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        
+        for ($i=0; $i < 10; $i++) { 
+            
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->company();
+            $newTrain->stazione_di_partenza = $faker->city();
+            $newTrain->stazione_di_arrivo = $faker->city();
+            $newTrain->orario_di_partenza = $faker->time();
+            $newTrain->orario_di_arrivo = $faker->time();
+            // $newTrain->codice_treno = $faker->randomNumber(9, true);
+            $newTrain->codice_treno = $faker->regexify('[A-Z]{5}[0-4]{3}');
+            $newTrain->numero_carrozze = $faker->randomDigit();
+            $newTrain->in_orario = $faker->boolean();
+            $newTrain->cancellato = $faker->boolean();
+            $newTrain->save();
+
+        }
+
+
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,30 @@
 <div class="container my-3">
     <h1>Welcome Page</h1>
     <div class="row g-4">
+        <h2>Treni</h2>
         <div class="col">
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, delectus ad esse illum omnis earum eligendi sint a minus quasi, inventore nulla autem. Maxime voluptatem eligendi veniam voluptates. Soluta, sunt!</p>
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Numero Treno</th>
+                        <th>Città Partenza</th>
+                        <th>Orario Partenza</th>
+                        <th>Città Arrivo</th>
+                        <th>Orario Arrivo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($trains as $train)
+                    <tr>
+                        <td>{{$train->codice_treno}}</td>
+                        <td>{{$train->stazione_di_partenza}}</td>
+                        <td>{{$train->orario_di_partenza}}</td>
+                        <td>{{$train->stazione_di_arrivo}}</td>
+                        <td>{{$train->orario_di_arrivo}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
